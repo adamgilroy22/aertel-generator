3637955232ff6292654384d13ec79348f1c0be14
Adam Gilroy <adamgilroy22@gmail.com>
2023-01-17 20:30:33 +0000

    Add changeHeadline function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,9 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 });
+
+// Headline
+function changeHeadline(){
+  let userInput = document.getElementById('news-headline').value;
+  document.getElementById('headline').innerHTML = userInput;
+}
